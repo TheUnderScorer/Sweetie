3161bf7f722c9c8552d548b944d44d70631f379e
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-08-09 09:24:31 +0200

    feat: Initial commit

---
+++ __tests__/App-test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from '../src/App';
+
+// Note: test renderer must be required after react-native.
+import { render } from '@testing-library/react-native';
+
+it('renders correctly', () => {
+  render(<App />);
+});
