76ba41d29b91c9c3fedd777519259e236f38a356
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-08-13 21:00:02 +0200

    chore: Add "makeStyles" utility

---
+++ src/styles/makeStyles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import { Theme } from 'react-native-paper/src/types';
+import { useThemeContext } from '../app/providers/ThemeProvider';
+import { useMemo } from 'react';
+
+type StylesCreator<
+  T extends StyleSheet.NamedStyles<T> | StyleSheet.NamedStyles<any>
+> = (theme: Theme) => T | StyleSheet.NamedStyles<T>;
+
+export const makeStyles = <
+  T extends StyleSheet.NamedStyles<T> | StyleSheet.NamedStyles<any>
+>(
+  stylesCreator: StylesCreator<T>,
+) => () => {
+  const { theme } = useThemeContext();
+
+  return useMemo(() => StyleSheet.create(stylesCreator(theme)), [theme]);
+};
